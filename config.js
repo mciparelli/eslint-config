0b21c02820a31faa8596500b85224dc00e552b58
Martin Ciparelli <mciparelli@gmail.com>
2016-10-07 15:43:50 -0300

    Disable prefer default export

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
     'no-void': 0,
     'consistent-return': [2, { 'treatUndefinedAsUnspecified': true }],
     'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0
   }
 };
