32c31ce7f7be9ee1af9c11de1961b140bd3e4182
Martin Ciparelli <mciparelli@gmail.com>
2016-10-13 19:17:22 -0300

    Add no-param-reassign rule

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ module.exports = {
   rules: {
     'comma-dangle': [2, 'never'],
     'new-cap': [2, {
-      'capIsNew': false
+      capIsNew: false
     }],
     'no-void': 0,
-    'consistent-return': [2, { 'treatUndefinedAsUnspecified': true }],
+    'consistent-return': [2, { treatUndefinedAsUnspecified: true }],
+    'no-param-reassign': [2, { props: false }],
     'import/no-unresolved': 0,
     'import/no-extraneous-dependencies': 0,
     'import/prefer-default-export': 0
