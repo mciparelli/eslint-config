2b0195da4e829ceb4d8e6d93776b8539dbb940db
Martin Ciparelli <mciparelli@gmail.com>
2016-11-08 09:39:11 -0300

    Enable param reassign, I dont care about mutating arguments object

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     }],
     'no-void': 0,
     'consistent-return': [2, { treatUndefinedAsUnspecified: true }],
-    'no-param-reassign': [2, { props: false }],
+    'no-param-reassign': 0,
     'import/no-unresolved': 0,
     'import/no-extraneous-dependencies': 0,
     'import/prefer-default-export': 0
