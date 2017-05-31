41789c4ca6c815e073e85d8b304b0c17762537cd
Martin Ciparelli <mciparelli@gmail.com>
2017-05-31 12:05:50 -0300

    Update rules

--- config.js
+++ config.js
@@ -8,8 +8,11 @@ module.exports = {
     'no-void': 0,
     'consistent-return': 0,
     'no-param-reassign': 0,
+    'no-plusplus': [0, { allowForLoopAfterthoughts: true }],
+    'arrow-parens': [0, 'as-needed'],
     'import/no-unresolved': 0,
     'import/no-extraneous-dependencies': 0,
-    'import/prefer-default-export': 0
+    'import/prefer-default-export': 0,
+    'import/extensions': [0, { js: 'never' }]
   }
 };
